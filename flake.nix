2f4eeaa1effabc88ac32c5c6ac0c87534e3aef49
aabccd021 <aabccd021@gmail.com>
2023-07-02 17:44:20 +0700

    better?

--- flake.nix
+++ flake.nix
@@ -4,8 +4,8 @@
   };
 
   outputs = { self, nixpkgs }: with nixpkgs.legacyPackages.x86_64-linux;
-  let
-    node_modules = buildNpmPackage {
+    let
+      nodeModules = buildNpmPackage {
         name = "node_modules";
         src = ./.;
         dontNpmBuild = true;
@@ -15,23 +15,23 @@
           cp -r node_modules $out
         '';
       };
-  in
+    in
     {
       devShell.x86_64-linux = mkShellNoCC {
-        buildInputs = [ nodejs bun ];
+        buildInputs = [ bun ];
         shellHook = ''
           if [[ -e ./node_modules ]]; then
             unlink ./node_modules
           fi
-          ln -s ${node_modules}/node_modules ./node_modules
+          ln -s ${nodeModules}/node_modules ./node_modules
         '';
       };
 
-      packages.x86_64-linux.default = buildNpmPackage {
+      packages.x86_64-linux.default = stdenv.mkDerivation {
         name = "my-package";
         src = ./.;
-        npmDepsHash = "sha256-alSzA+1eBokELdgCiOmIxn8/ThDkePoWaGowyWt4zPM=";
         buildPhase = ''
+          ln -s ${nodeModules}/node_modules ./node_modules
           ${bun}/bin/bun build src/index.ts --outfile ./dist/index.js
         '';
         installPhase = ''
