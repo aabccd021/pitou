03ec126dc58d4b50d4b197fc28b79172fd7f668d
aabccd021 <aabccd021@gmail.com>
2023-07-02 17:08:50 +0700

    node modules

--- flake.nix
+++ flake.nix
@@ -4,9 +4,27 @@
   };
 
   outputs = { self, nixpkgs }: with nixpkgs.legacyPackages.x86_64-linux;
+  let
+    node_modules = buildNpmPackage {
+        name = "node_modules";
+        src = ./.;
+        dontNpmBuild = true;
+        npmDepsHash = "sha256-alSzA+1eBokELdgCiOmIxn8/ThDkePoWaGowyWt4zPM=";
+        installPhase = ''
+          mkdir $out
+          cp -r node_modules $out
+        '';
+      };
+  in
     {
       devShell.x86_64-linux = mkShellNoCC {
         buildInputs = [ nodejs bun ];
+        shellHook = ''
+          if [[ -e ./node_modules ]]; then
+            unlink ./node_modules
+          fi
+          ln -s ${node_modules}/node_modules ./node_modules
+        '';
       };
 
       packages.x86_64-linux.default = buildNpmPackage {
