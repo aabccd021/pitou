a0239d37abe55fb59a7efe57f6e5e8fe53e04c43
aabccd021 <aabccd021@gmail.com>
2023-07-04 17:17:12 +0700

    works

--- flake.nix
+++ flake.nix
@@ -6,87 +6,95 @@
   outputs = { self, nixpkgs }: with nixpkgs.legacyPackages.x86_64-linux;
     let
 
-      packageLock = lib.trivial.pipe ./package-lock.json [
-        builtins.readFile
-        builtins.fromJSON
-      ];
+      npm2nix =
+        (packageLockPath: reload:
+          let
+            packageLock = lib.trivial.pipe
+              packageLockPath [
+              builtins.readFile
+              builtins.fromJSON
+            ];
 
-      tarballsFile = lib.trivial.pipe (packageLock.packages) [
-        ((lib.trivial.flip removeAttrs) [ "" ])
+            tarballsFile = lib.trivial.pipe (packageLock.packages) [
+              ((lib.trivial.flip removeAttrs) [ "" ])
 
-        builtins.attrValues
+              builtins.attrValues
 
-        (map (p: fetchurl {
-          url = p.resolved;
-          hash = p.integrity;
-        }))
+              (map (p: fetchurl {
+                url = p.resolved;
+                hash = p.integrity;
+              }))
 
-        (builtins.concatStringsSep "\n")
+              (builtins.concatStringsSep "\n")
 
-        (tarballs: writeTextFile {
-          text = tarballs + "\n";
-          name = "${packageLock.name}-${packageLock.version}-tarballs";
-        })
-      ];
+              (tarballs: writeTextFile {
+                text = tarballs + "\n";
+                name = "${packageLock.name}-${packageLock.version}-tarballs";
+              })
+            ];
 
-      nodeModules = stdenv.mkDerivation
-        {
-          pname = "${packageLock.name}-node-modules";
-          version = packageLock.version;
-          buildInputs = [ nodejs ];
-          dontUnpack = true;
-          buildPhase = ''
-            export HOME="$TMP/.home"
-            npm config set progress false
+            nodeModules = stdenv.mkDerivation {
+              pname = "${packageLock.name}-node-modules";
+              version = packageLock.version;
+              buildInputs = [ nodejs ];
+              dontUnpack = true;
+              buildPhase = ''
+                export HOME="$TMP/.home"
+                npm config set progress false
 
-            while read package
-            do npm cache add "$package"
-            done < ${tarballsFile}
+                while read package
+                do npm cache add "$package"
+                done < ${tarballsFile}
 
-            mkdir "$out"
-            cd "$out"
-            cp ${./package-lock.json} ./package-lock.json
-            npm ci --ignore-scripts --offline
-            rm package-lock.json
-          '';
-        };
+                mkdir "$out"
+                cd "$out"
+                cp ${packageLockPath} ./package-lock.json
+                npm ci --ignore-scripts --offline
+                rm package-lock.json
+              '';
+            };
 
-      setupNodeModules = ''
-        ln -sfn ${nodeModules}/node_modules ./node_modules
-      '';
+          in
+          {
+            setupNodeModules = ''
+              ln -sfn ${nodeModules}/node_modules ./node_modules
+            '';
 
-      npm = writeShellScriptBin "npm" ''
-        array_includes() {
-            local word=$1
-            shift
-            for el in "$@"; do [[ "$el" == "$word" ]] && return 0; done
-        }
+            npm = writeShellScriptBin "npm" ''
+              array_includes() {
+                  local word=$1
+                  shift
+                  for el in "$@"; do [[ "$el" == "$word" ]] && return 0; done
+              }
 
-        if ! array_includes install "$@" \
-           && ! array_includes uninstall "$@" \
-           && ! array_includes dedupe "$@" \
-           && ! array_includes ci "$@" \
-        ; then
-          ${nodejs}/bin/npm "$@" && exit 0
-        fi
+              if ! array_includes install "$@" \
+                 && ! array_includes uninstall "$@" \
+                 && ! array_includes dedupe "$@" \
+                 && ! array_includes ci "$@" \
+              ; then
+                ${nodejs}/bin/npm "$@" && exit 0
+              fi
           
-        unlink ./node_modules
-        ${nodejs}/bin/npm "$@" --package-lock-only
-        direnv reload
-      '';
+              unlink ./node_modules
+              ${nodejs}/bin/npm "$@" --package-lock-only
+              ${reload}
+            '';
+          }
+        );
 
+      npmPackages = npm2nix ./package-lock.json "direnv reload";
     in
     {
 
       devShell.x86_64-linux = mkShellNoCC {
-        buildInputs = [ bun npm ];
-        shellHook = setupNodeModules;
+        buildInputs = [ bun npmPackages.npm ];
+        shellHook = npmPackages.setupNodeModules;
       };
 
       packages.x86_64-linux.default = stdenv.mkDerivation {
         name = "my-package";
         src = ./.;
-        configurePhase = setupNodeModules;
+        configurePhase = npmPackages.setupNodeModules;
         buildPhase = "${bun}/bin/bun build src/index.ts --outfile ./dist/index.js";
         installPhase = ''
           mkdir $out
