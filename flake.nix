f6724b3d0a1efaa7980b49059de8089f3145d312
aabccd021 <aabccd021@gmail.com>
2023-07-04 17:30:07 +0700

    modularize

--- flake.nix
+++ flake.nix
@@ -5,84 +5,12 @@
 
   outputs = { self, nixpkgs }: with nixpkgs.legacyPackages.x86_64-linux;
     let
-
-      npm2nix =
-        (packageLockPath: reload:
-          let
-            packageLock = lib.trivial.pipe
-              packageLockPath [
-              builtins.readFile
-              builtins.fromJSON
-            ];
-
-            tarballsFile = lib.trivial.pipe (packageLock.packages) [
-              ((lib.trivial.flip removeAttrs) [ "" ])
-
-              builtins.attrValues
-
-              (map (p: fetchurl {
-                url = p.resolved;
-                hash = p.integrity;
-              }))
-
-              (builtins.concatStringsSep "\n")
-
-              (tarballs: writeTextFile {
-                text = tarballs + "\n";
-                name = "${packageLock.name}-${packageLock.version}-tarballs";
-              })
-            ];
-
-            nodeModules = stdenv.mkDerivation {
-              pname = "${packageLock.name}-node-modules";
-              version = packageLock.version;
-              buildInputs = [ nodejs ];
-              dontUnpack = true;
-              buildPhase = ''
-                export HOME="$TMP/.home"
-                npm config set progress false
-
-                while read package
-                do npm cache add "$package"
-                done < ${tarballsFile}
-
-                mkdir "$out"
-                cd "$out"
-                cp ${packageLockPath} ./package-lock.json
-                npm ci --ignore-scripts --offline
-                rm package-lock.json
-              '';
-            };
-
-          in
-          {
-            setupNodeModules = ''
-              ln -sfn ${nodeModules}/node_modules ./node_modules
-            '';
-
-            npm = writeShellScriptBin "npm" ''
-              array_includes() {
-                  local word=$1
-                  shift
-                  for el in "$@"; do [[ "$el" == "$word" ]] && return 0; done
-              }
-
-              if ! array_includes install "$@" \
-                 && ! array_includes uninstall "$@" \
-                 && ! array_includes dedupe "$@" \
-                 && ! array_includes ci "$@" \
-              ; then
-                ${nodejs}/bin/npm "$@" && exit 0
-              fi
-          
-              unlink ./node_modules
-              ${nodejs}/bin/npm "$@" --package-lock-only
-              ${reload}
-            '';
-          }
-        );
-
-      npmPackages = npm2nix ./package-lock.json "direnv reload";
+      npm2nix = import ./npm2nix.nix;
+      npmPackages = npm2nix {
+        inherit nixpkgs;
+        packageLockPath = ./package-lock.json;
+        reload = "direnv reload";
+      };
     in
     {
 
