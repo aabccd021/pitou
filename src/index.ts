6e3c09de215134594c1c7d72e8829d24482245ca
aabccd021 <aabccd021@gmail.com>
2023-07-05 20:22:29 +0700

    works

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,21 @@
 import Parser from 'web-tree-sitter';
 
-const main = async () => {
+type Language = 'nix' | 'javascript';
+
+const makeParser = async (lang: Language) => {
   await Parser.init();
   const parser = new Parser();
-  const Lang = await Parser.Language.load(`${import.meta.dir}/tree-sitter-javascript.wasm`);
+  const Lang = await Parser.Language.load(`${import.meta.dir}/tree-sitter-${lang}.wasm`);
   parser.setLanguage(Lang);
-  const tree = parser.parse('let x = 1;');
-  console.log(tree.rootNode.toString());
+  return parser;
+}
+
+const main = async () => {
+  const jsParser = await makeParser('javascript');
+  console.log(jsParser.parse('let x = 1').rootNode.toString())
+
+  const nixParser = await makeParser('nix');
+  console.log(nixParser.parse('key: _: key').rootNode.toString())
 }
 
 await main();
