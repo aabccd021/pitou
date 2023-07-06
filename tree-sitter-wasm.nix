6ffe06171996839a2190150481382193d3fa76e2
aabccd021 <aabccd021@gmail.com>
2023-07-06 13:20:18 +0700

    checkpoint

--- tree-sitter-wasm.nix
+++ tree-sitter-wasm.nix
@@ -2,66 +2,54 @@ pkgs: treeSitters:
 with pkgs;
 let
 
-  buildWasm = (key: { dirname, language, location, wasmName }:
+  buildWasm = (key: { dirname, language, location }:
     stdenv.mkDerivation {
       name = "${key}-wasm";
       dontUnpack = true;
       buildInputs = [ emscripten ];
       buildPhase = ''
-        cp -rH ${pkgs.tree-sitter.grammars}/${dirname}/${location}/. grammar
-        chmod +w grammar
-        ls grammar
-        ${tree-sitter}/bin/tree-sitter build-wasm grammar
+        cp -rH ${pkgs.tree-sitter.grammars}/${dirname}/. grammar
+        chmod -R +w grammar
+        cd grammar
+        cd ${location}
+        ls
+        ${tree-sitter}/bin/tree-sitter build-wasm .
       '';
       installPhase = ''
         mkdir $out
         ls
-        cp tree-sitter-${wasmName}.wasm $out
+        cp tree-sitter-${language}.wasm $out
       '';
     });
 in
 lib.trivial.pipe pkgs.tree-sitter.grammars [
   builtins.readDir
-  (builtins.mapAttrs (dirname: _:
-    let
-      language = lib.trivial.pipe dirname [
-        (lib.removePrefix "tree-sitter-")
-        (builtins.replaceStrings [ "-" ] [ "_" ])
-      ];
-    in
-    {
-      inherit language;
-      dirname = dirname;
-      location = ".";
-    })
-  )
+  (builtins.mapAttrs (dirname: _: { dirname = dirname; location = "."; }))
 
-  ((lib.trivial.flip removeAttrs) [
-    "tree-sitter-ocaml"
-    "tree-sitter-typescript"
-    "tree-sitter-llvm"
-    # "tree-sitter-perl"
-    # "tree-sitter-julia"
-    # "tree-sitter-vim"
-  ])
+  ((lib.trivial.flip removeAttrs) [ "tree-sitter-llvm" ])
 
   # https://github.com/NixOS/nixpkgs/blob/c99004f75fd28cc10b9d2e01f51a412d768269c8/pkgs/development/tools/parsing/tree-sitter/default.nix#L67-L74
   (grammars':
     grammars' //
-    # { tree-sitter-ocaml = grammars'.tree-sitter-ocaml // { location = "ocaml"; }; } //
-    # { tree-sitter-ocaml-interface = grammars'.tree-sitter-ocaml // { location = "interface"; }; } //
+    { tree-sitter-ocaml = grammars'.tree-sitter-ocaml // { location = "ocaml"; }; } //
+    { tree-sitter-ocaml-interface = grammars'.tree-sitter-ocaml // { location = "interface"; }; } //
     { tree-sitter-org-nvim = grammars'.tree-sitter-org-nvim // { language = "org"; }; } //
-    # { tree-sitter-typescript = grammars'.tree-sitter-typescript // { location = "typescript"; }; } //
-    # { tree-sitter-tsx = grammars'.tree-sitter-typescript // { location = "tsx"; }; } //
+    { tree-sitter-typescript = grammars'.tree-sitter-typescript // { location = "typescript"; }; } //
+    { tree-sitter-tsx = grammars'.tree-sitter-typescript // { location = "tsx"; }; } //
     { tree-sitter-markdown = grammars'.tree-sitter-markdown // { location = "tree-sitter-markdown"; }; } //
     { tree-sitter-markdown-inline = grammars'.tree-sitter-markdown // { language = "markdown_inline"; location = "tree-sitter-markdown-inline"; }; }
   )
 
-  (builtins.mapAttrs (_: grammar: grammar // { wasmName = grammar.language; }))
+  (builtins.mapAttrs (key: grammar: grammar // {
+    language = lib.trivial.pipe key [
+      (lib.removePrefix "tree-sitter-")
+      (builtins.replaceStrings [ "-" ] [ "_" ])
+    ];
+  }))
 
   # wasm fix
   (grammars': grammars' //
-    { tree-sitter-ql-dbscheme = grammars'.tree-sitter-ql-dbscheme // { wasmName = "dbscheme"; }; }
+    { tree-sitter-ql-dbscheme = grammars'.tree-sitter-ql-dbscheme // { language = "dbscheme"; }; }
   )
 
   (builtins.mapAttrs buildWasm)
