483cbcc2dccef09a575496b26094d13f89501d31
aabccd021 <aabccd021@gmail.com>
2023-07-06 14:17:31 +0700

    works

--- tree-sitter-wasm.nix
+++ tree-sitter-wasm.nix
@@ -1,32 +1,10 @@
 pkgs: treeSitters:
 with pkgs;
-let
 
-  buildWasm = (key: { dirname, language, location }:
-    stdenv.mkDerivation {
-      name = "${key}-wasm";
-      dontUnpack = true;
-      buildInputs = [ emscripten ];
-      buildPhase = ''
-        cp -rH ${pkgs.tree-sitter.grammars}/${dirname}/. grammar
-        chmod -R +w grammar
-        cd grammar
-        cd ${location}
-        ls
-        ${tree-sitter}/bin/tree-sitter build-wasm .
-      '';
-      installPhase = ''
-        mkdir $out
-        ls
-        cp tree-sitter-${language}.wasm $out
-      '';
-    });
-in
 lib.trivial.pipe pkgs.tree-sitter.grammars [
   builtins.readDir
-  (builtins.mapAttrs (dirname: _: { dirname = dirname; location = "."; }))
+  (builtins.mapAttrs (dirname: _: { dirname = dirname; }))
 
-  ((lib.trivial.flip removeAttrs) [ "tree-sitter-llvm" ])
 
   # https://github.com/NixOS/nixpkgs/blob/c99004f75fd28cc10b9d2e01f51a412d768269c8/pkgs/development/tools/parsing/tree-sitter/default.nix#L67-L74
   (grammars':
@@ -40,19 +18,57 @@ lib.trivial.pipe pkgs.tree-sitter.grammars [
     { tree-sitter-markdown-inline = grammars'.tree-sitter-markdown // { language = "markdown_inline"; location = "tree-sitter-markdown-inline"; }; }
   )
 
-  (builtins.mapAttrs (key: grammar: grammar // {
-    language = lib.trivial.pipe key [
-      (lib.removePrefix "tree-sitter-")
-      (builtins.replaceStrings [ "-" ] [ "_" ])
-    ];
-  }))
-
   # wasm fix
-  (grammars': grammars' //
-    { tree-sitter-ql-dbscheme = grammars'.tree-sitter-ql-dbscheme // { language = "dbscheme"; }; }
+  (grammars':
+    grammars' //
+    { tree-sitter-ql-dbscheme = grammars'.tree-sitter-ql-dbscheme // { wasmName = "dbscheme"; }; }
   )
+  ((lib.trivial.flip removeAttrs) [ "tree-sitter-llvm" ])
 
-  (builtins.mapAttrs buildWasm)
+  (builtins.mapAttrs (name: { location ? null, ... }@grammar:
+    let
+      wasmName = grammar.wasmName
+        or grammar.language
+        or (lib.trivial.pipe name [
+        (lib.removePrefix "tree-sitter-")
+        (builtins.replaceStrings [ "-" ] [ "_" ])
+      ]);
+    in
+    stdenv.mkDerivation {
+      name = "${name}-wasm";
+
+      src = pkgs.tree-sitter.grammars;
+
+      configurePhase = ''
+        cp -rH $src/${grammar.dirname}/. grammar
+        chmod -R +w grammar
+        cd grammar
+      '' + lib.optionalString (location != null) ''
+        cd ${location}
+      '';
+
+      nativeBuildInputs = [ emscripten ];
+
+      buildPhase = ''
+        runHook preBuild
+
+        ls
+        ${tree-sitter}/bin/tree-sitter build-wasm .
+
+        runHook postBuild
+      '';
+
+      installPhase = ''
+        runHook preInstall
+
+        mkdir $out
+        ls
+        cp tree-sitter-${wasmName}.wasm $out
+
+        runHook postInstall
+      '';
+    }))
+
   (builtins.attrValues)
   (builtins.concatStringsSep "\n")
   (treeSitterWasms: writeTextFile {
