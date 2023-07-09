b19e2d166fc69500cb2178ea16de765b7a25e97e
aabccd021 <aabccd021@gmail.com>
2023-07-10 06:57:33 +0700

    checkpoint

--- src/dev.ts
+++ src/dev.ts
@@ -1,9 +1,18 @@
 // example.ts
 import { withHtmlLiveReload } from "./hot";
+import { elementToString, h } from "./html";
+
+const html = h('html', {}, [
+  h('h1', {}, [
+    'Hello, world! yo'
+  ]),
+])
+
+const htmlString = `<!DOCTYPE html>\n${elementToString(html)}`
 
 export default withHtmlLiveReload({
   fetch: () => {
-    return new Response("<div>hell wor</div>", {
+    return new Response(htmlString, {
       headers: { "Content-Type": "text/html" },
     });
   },
---
+++ src/html.ts
@@ -0,0 +1,54 @@
+type Booleanish = boolean | 'true' | 'false';
+
+// https://github.com/DefinitelyTyped/DefinitelyTyped/blob/23c436771a06e6bb72edc427c57cfd36b686468a/types/react/index.d.ts#L1920-L1938
+type StandardAttributes = {
+  accesskey?: string | undefined;
+  autofocus?: boolean | undefined;
+  classname?: string | undefined;
+  contenteditable?: Booleanish | "inherit" | undefined;
+  contextmenu?: string | undefined;
+  dir?: string | undefined;
+  draggable?: Booleanish | undefined;
+  hidden?: boolean | undefined;
+  id?: string | undefined;
+  lang?: string | undefined;
+  nonce?: string | undefined;
+  placeholder?: string | undefined;
+  slot?: string | undefined;
+  spellcheck?: Booleanish | undefined;
+  style?: {} | undefined;
+  tabindex?: number | undefined;
+  title?: string | undefined;
+  translate?: 'yes' | 'no' | undefined;
+}
+
+type Attributes = {
+  html: StandardAttributes,
+  title: StandardAttributes,
+  h1: StandardAttributes,
+}
+
+type Element<Tag extends keyof Attributes> = {
+  tag: Tag,
+  attributes: Attributes[Tag],
+  children: (Element<keyof Attributes> | string)[],
+}
+
+export const h = <Tag extends keyof Attributes>(
+  tag: Tag,
+  attributes: Attributes[Tag],
+  children: (Element<keyof Attributes> | string)[]
+): Element<Tag> => ({ tag, attributes, children })
+
+export const elementToString = <Tag extends keyof Attributes>(
+  element: Element<Tag> | string
+): string => {
+  if (typeof element === 'string') {
+    return element;
+  }
+  const attributes = Object.entries(element.attributes)
+    .map(([key, value]) => `${key}="${value}"`)
+    .join(' ');
+  const children = element.children.map(elementToString).join('');
+  return `<${element.tag} ${attributes}>\n  ${children}\n</${element.tag}>`;
+}
